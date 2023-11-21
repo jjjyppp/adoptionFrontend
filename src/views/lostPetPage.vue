a0181178ff3d6fa66e2a5fa3df934e3431771dd9
wangchunchia <wangchunchia@outlook.com>
2023-11-21 23:37:10 +0800

    lost pet page

--- src/views/lostPetPage.vue
+++ src/views/lostPetPage.vue
@@ -1,13 +1,36 @@
 <script setup>
-
+import {ElTag} from "element-plus";
 import HeaderTag from "@/components/HeaderTag.vue";
+import lostPetCard from "@/components/lostPetCard.vue";
 </script>
 
 <template>
   <header-tag></header-tag>
-  <h1>lost pet page</h1>
+  <el-tag class="tag" size="large">寻找宠物</el-tag>
+  <el-tag class="tag" size="large" color="lightgreen">寻找主人</el-tag>
+  <el-tag class="tag" size="large" color="orange">丢寻登记</el-tag>
+  <div class="container">
+    <lost-pet-card></lost-pet-card>
+    <lost-pet-card></lost-pet-card>
+    <lost-pet-card></lost-pet-card>
+    <lost-pet-card></lost-pet-card>
+    <lost-pet-card></lost-pet-card>
+    <lost-pet-card></lost-pet-card>
+    <lost-pet-card></lost-pet-card>
+    <lost-pet-card></lost-pet-card>
+  </div>
+
 </template>
 
 <style scoped>
+.tag {
+  margin: 30px;
+}
+.container{
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: 60px;
+  margin-top: 20px;
+}
 
 </style>
