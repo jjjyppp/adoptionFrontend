e2c1eb4c8a0b365e2fefc0aa3a05c11a55e96cb4
chenhongyu <1428467470@qq.com>
2023-11-28 16:54:37 +0800

    封装了footer，from chenhongyu

---
+++ src/components/FooterCard.vue
@@ -0,0 +1,117 @@
+<template>
+  <footer>
+    <!--Footer top strat-->
+    <section id="footer-top" class="py-4">
+      <div class="container">
+        <div class="row pt-4">
+          <div class="col-md-2 col-sm-6">
+            <div class="footer-inner">
+              <h5 class="py-2">关于</h5>
+              <ul class="px-0">
+                <a href="#">
+                  <li class="m-0">关于我们</li>
+                </a>
+                <a href="#">
+                  <li>合作单位</li>
+                </a>
+                <a href="#">
+                  <li>新闻</li>
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-6">
+            <div class="footer-inner">
+              <h5 class="py-2">关于宠物</h5>
+              <ul class="px-0">
+                <a href="#">
+                  <li>收养宠物</li>
+                </a>
+                <a href="#">
+                  <li>科学养宠科普</li>
+                </a>
+                <a href="#">
+                  <li>宠物行为</li>
+                </a>
+                <a href="#">
+                  <li>宠物健康</li>
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-6">
+            <div class="footer-inner">
+              <h5 class="py-2">公益事业</h5>
+              <ul class="px-0">
+                <a href="#">
+                  <li>领养或救助</li>
+                </a>
+                <a href="#">
+                  <li>流浪动物收容所</li>
+                </a>
+                <a href="#">
+                  <li>绝育行动</li>
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-6">
+            <div class="footer-inner">
+              <h5 class="py-2">联系电话</h5>
+              <p>
+                +15094378007
+              </p>
+              <h5 class="py-2">地址</h5>
+              <p>
+                南京大学鼓楼校区
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="sign">
+              <h5 class="py-2">关爱动物，科学养宠，我们在行动</h5>
+              <a class="text-center d-block py-5"><span class="py-2 px-4 fs-5"> 登录</span></a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!--Footer top end-->
+
+    <!--Footer bottom start-->
+    <section id="footer-bottom" class="py-3">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col-md-6">
+            <p class="mb-0">
+              ©2023 Petfinder.com All trademarks are owned by nju, or used
+              with permission.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <div class="social-media text-center">
+              <a href="#"><span><i class="fa-brands fa-facebook-f"></i></span></a>
+              <a href="#"><span><i class="fa-brands fa-twitter"></i></span></a>
+              <a href="#"><span><i class="fa-brands fa-instagram"></i></span></a>
+              <a href="#"><span><i class="fa-brands fa-youtube"></i></span></a>
+              <a href="#"><span><i class="fa-brands fa-pinterest-p"></i></span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--Footer bottom end-->
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterCard"
+}
+</script>
+
+<style scoped>
+@import url('src/views/assets/css/plugin.css');
+@import url('src/views/assets/css/main.css');
+</style>
