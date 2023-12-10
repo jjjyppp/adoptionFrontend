ec46c4ea21a2815cd74e9cef4fd2a86c6f9c22c2
chenhongyu <1428467470@qq.com>
2023-12-10 22:09:38 +0800

    完成了注册和登录页面的逻辑，现在可以登录了喵~from chenhongyu

--- src/views/loginPage.vue
+++ src/views/loginPage.vue
@@ -12,10 +12,10 @@ import FooterCard from "@/components/FooterCard.vue";
           <div class="col-12 col-lg-6 text-center ">
             <div class="log-inner py-5">
               <h1 class="py-2"> 登录</h1>
-              <input type="email" name="" id="" placeholder="Email" required>
-              <input type="password" name="" id="" placeholder="password" required>
-              <a href="#" ><button class="py-2 px-5 fs-5 my-2">LOG IN</button></a>
-              <p >尚未注册账号？<a href="./">去注册</a></p>
+              <input type="text" name="username" v-model.trim="name" id="" placeholder="用户名"  required>
+              <input type="password" name="password" v-model.trim="password" id="" placeholder="password" required>
+              <a href="#" ><button class="py-2 px-5 fs-5 my-2" @click.prevent="handlelogin">LOG IN</button></a>
+              <p >尚未注册账号？<a href='' @click.prevent="handleregister">去注册</a></p>
             </div>
 
           </div>
@@ -24,6 +24,47 @@ import FooterCard from "@/components/FooterCard.vue";
     </section>
   <footer-card></footer-card>
  </template>
+<script>
+export default {
+  data(){
+    return{
+      name:"",//姓名，用v-model绑定监听，将输入的字符串赋值给name变量
+      password:"",//密码
+      st:"false",//false为不保存登录
+    };
+  },
+  methods:{
+    handlelogin:function()
+    {
+      if(this.name===localStorage['name'] && this.password===localStorage['password'])
+      {
+        this.$router.replace('/');//如果输入的名字以及密码正确路由跳转至home
+      }
+      else if(this.name==='')//名字为空
+      {
+        alert('用户名不为空');
+      }
+      else if(this.password==='')//密码为空
+      {
+        alert('密码不为空');
+      }
+      else{
+        alert('账号不存在，请注册后登录');//查无此号
+      }
+    },
+    handleregister:function()
+    {
+      this.$router.replace('/signup')//点击注册按钮，跳转至注册页面
+    },
+    //点击保持登录状态触发handlesave
+    handlesave:function(){
+      this.st="true";//修改登录状态为true
+      localStorage.setItem('s',this.st);
+      console.log(localStorage.s);
+    }
+  }
+};
+</script>
 
 <style>
 @import url('./assets/css/main.css');
--- src/views/signupPage.vue
+++ src/views/signupPage.vue
@@ -10,26 +10,26 @@
             <div class="sign-inner text-center py-4 my-4">
               <h1 class="pb-3">注册</h1>
               <form action="">
-                <input type="text" placeholder="真实姓名">
-                <input type="text" placeholder="身份证号">
-                <input type="number" placeholder="手机号">
-                <input type="text" placeholder="用户名">
-
+                <input type="text" placeholder="真实姓名" v-model.trim="realname">
+                <input type="text" placeholder="身份证号" v-model.trim="idnumber">
+                <input type="tel" placeholder="手机号" v-model.trim="tel">
+                <input type="email" placeholder="邮箱" v-model.trim="mail">
+                <input type="text" placeholder="用户名" v-model.trim="name">
                 <div class="position-relative d-inline">
-                  <input type="password" placeholder="设置密码" id="pwd" >
+                  <input type="password" placeholder="设置密码" id="pwd" v-model.trim="password">
                   <i class="fa fa-lock fa-lg"></i>
                   <i class="fa fa-eye-slash fa-lg" v-if="!psw" aria-hidden="true"></i>
                   <i class="fa fa-eye fa-lg" aria-hidden="true" @click="psw=!psw"></i>
                 </div>
                  <span class="position-relative d-inline">
-                  <input type="password" placeholder="再次输入您的密码" id="password" >
+                  <input type="password" placeholder="再次输入您的密码" id="password" v-model.trim="password2">
                   <span class="position-absolute top-0 end-0 mx-2 " id="password-icon">
                     <i class="fa fa-lock fa-lg"></i>
                     <i class="fa fa-eye-slash fa-lg" v-if="!psw" aria-hidden="true"></i>
                     <i class="fa fa-eye fa-lg" aria-hidden="true" @click="psw=!psw"></i>
                     </span></span>
-                <a href="#"><button class="py-2 px-5 fs-5 my-2">注册</button></a>
-                <p class="py-3" >已有账号？<a class="text">去登录</a></p>
+                <a href="#"><button class="py-2 px-5 fs-5 my-2" @click.prevent="handlefinish">注册</button></a>
+                <p class="py-3" >已有账号？<a href='' @click.prevent="handlelogin">去登录</a></p>
 
               </form>
 
@@ -38,21 +38,63 @@
         </div>
       </div>
     </section>
+
+
   <footer-card></footer-card>
 </template>
 
 <script>
+
 import FooterCard from "@/components/FooterCard.vue";
 export default {
   components: {FooterCard},
+  name:'register',
+  props: {
+    msg: String
+  },
   data () {
     return {
-      psw: true,
-      userCode: '',
-      userPwd: ''
-    }
+      realname:"",
+      idnumber:"",
+      tel:"",
+      mail:"",
+      name:"",
+      password1:"",
+      password2:""
+    };
+  },methods:{
+    handlefinish:function()
+    {
+      if(localStorage['name']===this.name)
+      {
+        alert("用户名已存在");//如果用户名已存在则无法注册
+      }
+      else if(this.name==='')
+      {
+        alert("用户名不能为空");
+      }
+      if(this.password!==this.password2){
+        alert("两次密码不相同");
+      }
+      else{//将新用户信息存储到localStorage
+        localStorage.setItem('realname',this.realname);
+        localStorage.setItem('idnumber',this.idnumber);
+        localStorage.setItem('name',this.name);
+        localStorage.setItem('password',this.password);
+        localStorage.setItem('mail',this.mail);
+        localStorage.setItem('tel',this.tel);
+        localStorage.setItem('s',"false");
+        alert("注册成功");
+        this.$router.replace('/login');//完成注册后跳转至登录页面
+      }
+    },
+    handlelogin:function()
+    {
+      this.$router.replace('/login')
+    },
+
   }
-}
+};
 </script>
 
 
